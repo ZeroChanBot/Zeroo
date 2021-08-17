b12d4162c2d602b813e82cccaff1f6999b806d70
ZEROBOT <88374559+ZeroChanBot@users.noreply.github.com>
2021-08-18 00:07:45 +0700

    Update _expired.js

--- plugins/_expired.js
+++ plugins/_expired.js
@@ -4,8 +4,8 @@ handler.before = async (m) => {
     if (m.isGroup && global.DATABASE.data.chats[m.chat].expired != 0) {
         if (new Date() * 1 >= global.DATABASE.data.chats[m.chat].expired) {
             let users = (await this.groupMetadata(m.chat)).participants.map(u => u.jid)
-            this.reply(m.chat, `waktunya *${this.user.name}* untuk meninggalkan grup`, null, { contextInfo: { mentionedJid: users } }).then(() => {
-                this.sendContact(m.chat, global.owner[0], 'BENNIISMAEL', m).then(() => {
+            this.reply(m.chat, `Waktunya *${this.user.name}* Untuk Meninggalkan Grup Anda`, null, { contextInfo: { mentionedJid: users } }).then(() => {
+                this.sendContact(m.chat, global.owner[0], 'ZEROBOT, m).then(() => {
                     this.groupLeave(m.chat).then(() => {
                         global.DATABASE.data.chats[m.chat].expired = 0
                     })
