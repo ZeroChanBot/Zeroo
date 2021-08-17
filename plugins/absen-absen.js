a1aa23e2a538cbf17f88b6d13e96dd32da19127d
ZEROBOT <88374559+ZeroChanBot@users.noreply.github.com>
2021-08-18 00:09:00 +0700

    Update absen-absen.js

--- plugins/absen-absen.js
+++ plugins/absen-absen.js
@@ -27,10 +27,10 @@ ${list}
 │ 
 └────
 
-_By Raku Gans_`, m, { contextInfo: { mentionedJid: absen } })
+_ZEROBOT`, m, { contextInfo: { mentionedJid: absen } })
 }
-handler.help = ['.absen']
+handler.help = ['absen']
 handler.tags = ['abs']
-handler.command = ['.absen']
+handler.command = ['absen']
 handler.group = true
 module.exports = handler
